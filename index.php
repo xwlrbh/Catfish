801d6f1be3896bbf9c6bd1248d4b76ab645a5d31
A.J <xwlrbh@sina.cn>
2016-12-07 11:53:43 +0800

    no message

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
  * Project: Catfish.
  * Author: A.J
@@ -6,5 +6,5 @@
  */
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/application/');
-// 加载框架引导文件
+// 加载引导文件
 require __DIR__ . '/catfish/start.php';
